12651c878441ffcede63f218f31fa7f129a94775
Jaime Ibarra <utm22030687@utma.edu.mx>
2023-05-31 23:52:18 -0600

    first commit

---
+++ exer_pyt.py
@@ -0,0 +1,6 @@
+operations = ['addition', 'subtraction', 'multiplication', 'division', 'square root']# Create a list
+print("This is the list of operations ") #initial message
+operations.append('exponent')# New element on the list
+for item in range(1): # Elements using a for loop
+    print(operations) #result
+print("-------------------------------------------------")#Its a division
