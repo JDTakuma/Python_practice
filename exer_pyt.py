d55e305bd1fd4f16fdf3a8c92770544b112a5e92
Jaime Ibarra <utm22030687@utma.edu.mx>
2023-06-01 00:36:07 -0600

    feat

--- exer_pyt.py
+++ exer_pyt.py
@@ -4,3 +4,10 @@ operations.append('exponent')# New element on the list
 for item in range(1): # Elements using a for loop
     print(operations) #result
 print("-------------------------------------------------")#Its a division
+fruits = ('apple','banana','orange','grape','watermelon','kiwi', 'pineapple') #list of fruits
+length = len(fruits)#check the number of fruits on the list
+i = 0 #Counter
+print("The seven fruits are:")#Message 
+while i < length: #loop to be followed until the condition is not met
+    print(fruits[i])#will print the 7 fruits in different lines 
+    i += 1
